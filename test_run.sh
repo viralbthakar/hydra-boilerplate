00365c718e71c7b3f96b6e0b693d4aeb6d94d1b0
viralbthakar <viralbthakar@gmail.com>
2023-07-11 14:51:28 -0400

    Updated

--- test_run.sh
+++ test_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # execute explore_mini_batch_selection.py ten times
-python src/main.py --multirun hydra/launcher=joblib hydra.launcher.n_jobs=6 experiment_config.repetitions_cnt=1 training_config.batch_size=16,32,64  training_config.epochs=30 model_config.kernel_initializer=he-uniform,rnd-uniform
+python src/main.py --multirun hydra/launcher=joblib hydra.launcher.n_jobs=8 experiment_config.repetitions_cnt=3 training_config.batch_size=16,32,64  training_config.epochs=5 model_config.kernel_initializer=he-uniform,rnd-uniform &&
+python src/stats.py
